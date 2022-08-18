a27072978f4786d0592e329f9d0d1de886dcab9b
santiago <santiago.borquez.1705@gmail.com>
2022-08-18 17:55:35 -0300

    Fixing problem with vite & env variables. SB

--- client/src/redux/api/productsAPI.ts
+++ client/src/redux/api/productsAPI.ts
@@ -9,7 +9,7 @@ type product = {
     id: string
 }
 
-const baseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001'
+const baseUrl = import.meta.env.VITE_REACT_APP_API || 'http://localhost:3001'
 
 export const productsAPI = createApi({
     reducerPath: 'productsAPI',
